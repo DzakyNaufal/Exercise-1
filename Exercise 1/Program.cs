5a714d91b842c822be53bba75d4d4112626e129f
DzakyNaufal <dzakynmulna@gmail.com>
2022-10-12 17:43:21 +0700

    Add project files.

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleShort
+{
+    class Program
+    {
+        // Deklrasi array int dengan ukuran 53
+        private int[] dzaky = new int[53];
+
+        // Deklarasi variabel int untuk menyimpan banyaknya data yang disimpan pada array
+        private int n;
+
+        // fungsi / method untuk menerima masukan
+        public void read()
+        {
+            //menerima angka untuk menentukan banyaknya data yang disimpan pada array 
+            while (true)
+            {
+                Console.Write("Masukkan Banyaknya Elemen Pada Array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 53)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 53 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(" Masukkan Elemen Array ");
+            Console.WriteLine("-----------------------");
+
+            // Pengguna memasukkan elemen pada array
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                dzaky[i] = Int32.Parse(s1);
+            }
+
+        }
+        public void display()
+        {
+            // Menampilkan array yang tersusun
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(" Element array yang telah tersusun ");
+            Console.WriteLine("-----------------------------------");
+            for (int DN = 0; DN < n; DN++)
+            {
+                Console.WriteLine(dzaky[DN]);
+            }
+            Console.WriteLine("");
+        }
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++) // for n - 1 passes
+            {
+                //pada pass i, bandingkan n - 1 elemen pertama dengan elemen selanjutnya
+                for (int DN = 0; DN < n; DN++)
+                {
+                    if (dzaky[DN] < dzaky[DN + 1]) // jika elmen tidak dalam urutan yang benar
+                    {
+                        // tukar elemen
+                        int temp;
+                        temp = dzaky[DN];
+                        dzaky[DN] = dzaky[DN + 1];
+                        dzaky[DN + 1] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            // creating the object of the BubbleSort class
+            Program mylist = new Program();
+
+            //pemanggilan fungsi untuk menerima elemen array
+            mylist.read();
+
+            //pemanggilan fungsi untuk mengurutkan array 
+            mylist.BubbleSortArray();
+
+            //pemanggilan fungsi untuk menampilkan array yang tersusun
+            mylist.display();
+
+            //exit
+            Console.WriteLine("\n\n Tekan tombol apa saja untuk keluar.");
+            Console.Read();
+        }
+    }
+
+}
